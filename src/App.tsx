55150b9bcd7431dc28901205ac039ee446e6ae32
Katalina Okano <git@kat.cafe>
2022-11-11 12:06:04 -0500

    maps scheme test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { styled, useStyletron } from "baseui";
+import { AppNavBar } from "baseui/app-nav-bar";
+import { Button } from "baseui/button";
+import { ArrowRight, Search } from "baseui/icon";
+import { Input } from "baseui/input";
+import { DisplayXSmall, ParagraphSmall } from "baseui/typography";
+import React from "react";
 
-function App() {
+const Centered = styled("div", {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100%",
+  flexDirection: "column",
+});
+
+const Wide = styled("div", {
+  width: "1000px",
+  maxWidth: "90vw",
+  flex: 1,
+  textAlign: "center",
+});
+
+const Info = styled("span", {
+  textDecoration: "dotted underline",
+});
+
+export default function App() {
+  const [css] = useStyletron();
+  const [search, setSearch] = React.useState("");
+  const placeholder = React.useMemo(() => {
+    const list: string[] = [
+      "McDonald's",
+      "Burger King",
+      "Taco Bell",
+      "Walgreens",
+      "Walmart",
+      "Target",
+      "7-11",
+      "CVS",
+      "Sheetz",
+      "QuikTrip",
+      "Circle K",
+      "Gas Station",
+      "Starbucks",
+      "Restaurant",
+      "Grocery Store",
+      "Pharmacy",
+      "City Hall",
+      "Courthouse",
+      "Police Station",
+    ];
+
+    return list[Math.floor(Math.random() * list.length)];
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppNavBar title={"NPC Driving School"} />
+      <Centered>
+        <Wide>
+          <DisplayXSmall marginBottom="scale500" marginTop="scale500">
+            Where should I send you?
+          </DisplayXSmall>
+        </Wide>
+        <Wide className={css({ display: "flex" })}>
+          <Input
+            value={search}
+            placeholder={placeholder}
+            onChange={(e) => setSearch(e.target.value)}
+            endEnhancer={() => <Search />}
+          />
+          <Button
+            $as="a"
+            href={`maps://?q=${search || placeholder}`}
+            endEnhancer={() => <ArrowRight />}
+          >
+            Navigate
+          </Button>
+        </Wide>
+        <Wide>
+          <ParagraphSmall>
+            NPC Driving School will randomly pick a map destination based on
+            your search. You'll get a Google Maps or Apple Maps link to follow.
+          </ParagraphSmall>
+          <ParagraphSmall>
+            Destinations will be within{" "}
+            <Info title="In other words, it doesn't take roads in account.">
+              15 miles (as the crow flies)
+            </Info>{" "}
+            of your location. It's recommended to turn off highways and tolls.
+          </ParagraphSmall>
+          <ParagraphSmall>
+            Use this app at your own risk. Be safe. Don't drive intoxicated. Be
+            aware of your surroundings, and don't drive distracted.
+          </ParagraphSmall>
+        </Wide>
+      </Centered>
+    </>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BaseProvider, DarkTheme } from "baseui";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const engine = new Styletron();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={DarkTheme}>
+        <App />
+      </BaseProvider>
+    </StyletronProvider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
